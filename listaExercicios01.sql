98963eafc05736516060d31f87acd8dc906e966e
KayqueHenrry <Kayquehenrryfo@gmail.com>
2023-09-09 23:27:27 -0300

    Selecionei os nomes dos autores, cujo a data de nascimento é antes de 1900

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,1 +1,4 @@
-select titulos from livros
+select titulos from livros;
+select nomes from autores where nascimento < 1900;
+SELECT titulo FROM livros
+WHERE autor_id = (SELECT id FROM autores WHERE nome = 'J.K. Rowling');
