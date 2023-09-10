8badc90113586b40798bcfbb489c3c30d76c2097
KayqueHenrry <Kayquehenrryfo@gmail.com>
2023-09-10 20:21:22 -0300

    Mensagem do seu commit aqui

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -1,4 +1,3 @@
+use aula_db_exemplos;
 select titulos from livros;
 select nomes from autores where nascimento < 1900;
-SELECT titulo FROM livros
-WHERE autor_id = (SELECT id FROM autores WHERE nome = 'J.K. Rowling');
