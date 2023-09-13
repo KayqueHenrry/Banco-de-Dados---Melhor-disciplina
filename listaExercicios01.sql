3384e3eef2381a5efb154c60c38b74a8b5500b2f
KayqueHenrry <102825082+KayqueHenrry@users.noreply.github.com>
2023-09-12 21:50:25 -0300

    Exercício 03

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -4,3 +4,8 @@ select titulos from livros;
 
  --02
 select nomes from autores where nascimento < '1900-01-01';
+
+--03
+SELECT titulo FROM livros
+INNER JOIN autores ON livros.autor_id = autores.id
+WHERE autores.nome = 'J.K. Rowling';
