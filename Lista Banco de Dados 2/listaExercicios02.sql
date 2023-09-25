8110442432bccc243c4a9439125394619945ea90
KayqueHenrry <102825082+KayqueHenrry@users.noreply.github.com>
2023-09-25 20:26:44 -0300

    Exercício 02

--- Lista Banco de Dados 2/listaExercicios02.sql
+++ Lista Banco de Dados 2/listaExercicios02.sql
@@ -1,5 +1,15 @@
--- Ex. 1
+-- Ex. 01
 CREATE PROCEDURE sp_ListarAutores()
 BEGIN
-SELECT * FROM Autor;
+  SELECT * FROM Autor;
 END;
+
+-- Ex. 02
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END;
+
